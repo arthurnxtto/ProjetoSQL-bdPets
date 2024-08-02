bf122dd094a5d263f28aa803a9e6dc5a1d2c4358
arthurnxtto <arthurvnetto8@outlook.com>
2024-08-02 20:16:57 -0300

    Script SQL

--- dbPets.sql
+++ dbPets.sql
@@ -1,8 +1,31 @@
--- Apagar Banco de Dados
+-- apagar o banco de dados
 drop database dbPets;
--- Criar Banco de Dados
+-- criar banco de dados
 create database dbPets;
--- Acessar o Banco de Dados
+-- acessar o banco de dados
 use dbPets;
--- Visualizando banco de dados
+-- visualizando banco de dados
 show databases;
+-- criando tabelas no banco de dados
+create table tbFuncionarios(
+codFunc int not null auto_increment,
+nome varchar(100) not null,
+email varchar(100),
+telCel char(10),
+cpf char(14),
+primary key (codFunc)
+);
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(50) not null,
+senha varchar(10) not null,
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc)references tbFuncionarios(codFunc)
+);
+
+-- visualizando tabelas
+show tables;
+-- visualizando a estrutura das tabelas
+desc tbUsuarios;
+desc tbFuncionarios;

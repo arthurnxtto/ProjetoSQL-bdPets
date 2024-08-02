b82476319d101bfa882c73fcf45ebdee784a5080
arthurnxtto <arthurvnetto8@outlook.com>
2024-08-02 19:28:10 -0300

    Script SQL Pets

---
+++ dbPets.sql
@@ -0,0 +1,8 @@
+-- Apagar Banco de Dados
+drop database dbPets;
+-- Criar Banco de Dados
+create database dbPets;
+-- Acessar o Banco de Dados
+use dbPets;
+-- Visualizando banco de dados
+show databases;
